0072448ba7b3a169b1b0e7f751a714b381a77d4b
Fatima Garcia <fagarmartin@gmail.com>
2023-05-03 17:49:30 +0200

    Lab hecho, empezar CSS

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { error } = require('console');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -9,6 +10,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials')); // registrar parciales
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -21,5 +23,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+async function getBeersAsync(response) {
+  try {
+    const beerArr = await punkAPI.getBeers();
+    console.log('beerArr', beerArr);
+    response.render('beers', { beerArr });
+  } catch (error) {
+    console.log(error);
+  }
+}
 
+app.get('/beers', (req, res) => {
+  getBeersAsync(res);
+});
+app.get('/beers/beer/:id', (req, res) => {
+ 
+  punkAPI.getBeer(req.params.id).then((response)=>{
+    console.log('response',response);
+      res.render('random-beer', { randomBeer: response[0] }); // reuso el de random beer
+    
+  });
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(response => {
+      //este metodo devuelve un array
+      console.log(response, 'response');
+      res.render('random-beer', { randomBeer: response[0] });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
